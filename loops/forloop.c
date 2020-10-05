082540d13e5fdf6c0d42f6f699a38eb1b5fff6b2
Victor <35284183+vic7z@users.noreply.github.com>
2020-10-05 14:21:30 +0530

    Update forloop.c

--- loops/forloop.c
+++ loops/forloop.c
@@ -5,6 +5,7 @@ int main()
     int num,i;
     printf("Enter a number :");
     scanf("%d", &num);
+    //for loop
     for ( i = 1; i <=10; i++)
     {
         printf("%d * %d = %d\n", i,num,i*num );
