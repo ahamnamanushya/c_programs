f9cc61f65c1351d1e62442af4323223be01ddf63
vic <touchwithvictor@gmail.com>
2020-10-30 15:34:53 +0530

    switch

--- 2d array/2darray.c
+++ 2d array/2darray.c
@@ -1,20 +1,25 @@
-#include<stdio.h>S
+#include<stdio.h>
 
 int main(){
 
-    int num[5][5]={{1,2,3,4,5},{3,324,3443,34,4},{3,43,434,4,3},{23,43,34,3,4}};
+    int num[5][5]={{1,2,3,4,5},
+                    {3,324,3443,34,4},
+                    {3,43,434,4,3},
+                    {23,43,34,3,4}};
     int num1[5][5];
     int sum=0;
 
     
     for (int i = 0; i <5; i++) //row
-    {
+    {  
+      if(i==4){
+           printf("\n");
+      0}
+
        for (int j = 0; j < 5; j++) //column
        {
          sum=num[i][j]+num1[i][j];
-         if(i==4){
-           printf("\n");
-         }
+       
          printf("%d\t",num[i][j]);
        }
        
